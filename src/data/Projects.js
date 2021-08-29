0de2310ec25fd2db6b064d8338dd510f33536641
DenseEclipse <bajiyaharish20@gmail.com>
2021-08-29 22:30:32 +0530

    Project Added, Resolved Masonry Issue

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -11,6 +11,14 @@
 const projects = [
     {
         "id": 1,
+        "title": "Friday Finance Season 2",
+        "image": require("../assets/images/ffs2.jpg"),
+        "link": "https://youtu.be/7Av-H3xZSJQ",
+        "category": "Commercial",
+        "Job": "2nd Unit Director of Photography & 1st Assistant Cinematographer",
+    },
+    {
+        "id": 2,
         "title": "Hyatt Regency",
         "image": require("../assets/images/hyatt.png"),
         "link": "https://youtu.be/l20jQEAnKhY",
@@ -18,7 +26,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 2,
+        "id": 3,
         "title": "MG Hector",
         "image": require("../assets/images/mghector.png"),
         "link": "https://www.instagram.com/p/CHc73kXFZNO/?igshid=zulfyhw2msxh",
@@ -26,7 +34,7 @@ const projects = [
         "Job": "Assistant Cinematographer",
     },
     {
-        "id": 3,
+        "id": 4,
         "title": "MG Gloster",
         "image": require("../assets/images/mggloster.png"),
         "link": "https://www.instagram.com/p/CHfUtI0lOQg/?igshid=1i48sw2xvd6mg",
@@ -34,7 +42,7 @@ const projects = [
         "Job": "Assistant Cinematographer",
     },
     {
-        "id": 4,
+        "id": 5,
         "title": "MG Hector 2",
         "image": require("../assets/images/mghector3.png"),
         "link": "https://www.instagram.com/p/CHh3p6Kl4eY/?igshid=fzyrzgu8ioqi",
@@ -42,7 +50,7 @@ const projects = [
         "Job": "Assistant Cinematographer",
     },
     {
-        "id": 5,
+        "id": 6,
         "title": "MG ZS EV",
         "image": require("../assets/images/mgzsev.png"),
         "link": "https://www.instagram.com/p/CHj0ptClxlb/?igshid=s1fvwsjto0yq",
@@ -50,7 +58,7 @@ const projects = [
         "Job": "Assistant Cinematographer",
     },
     {
-        "id": 6,
+        "id": 7,
         "title": "B Roll 1 (DUBAI) ",
         "image": require("../assets/images/broll1.png"),
         "link": "https://sarthakgogna5.wixsite.com/website/work?wix-vod-video-id=f0255ada56264333a915460fbfb3afa6&wix-vod-comp-id=comp-jiwy9miy",
@@ -58,7 +66,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 7,
+        "id": 8,
         "title": "B Roll 2 (DUBAI) ",
         "image": require("../assets/images/broll2.png"),
         "link": "https://sarthakgogna5.wixsite.com/website/work?wix-vod-video-id=f0255ada56264333a915460fbfb3afa6&wix-vod-comp-id=comp-jiwy9miy",
@@ -66,7 +74,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 8,
+        "id": 9,
         "title": "B Roll 3 (DUBAI) ",
         "image": require("../assets/images/broll3.png"),
         "link": "https://sarthakgogna5.wixsite.com/website/work?wix-vod-video-id=f0255ada56264333a915460fbfb3afa6&wix-vod-comp-id=comp-jiwy9miy",
@@ -74,7 +82,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 9,
+        "id": 10,
         "title": "Littleens Campaign Video (Boys) ",
         "image": require("../assets/images/littleens1.png"),
         "link": "https://sarthakgogna5.wixsite.com/website/work?wix-vod-video-id=f0255ada56264333a915460fbfb3afa6&wix-vod-comp-id=comp-jiwy9miy",
@@ -82,7 +90,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 10,
+        "id": 11,
         "title": "Littleens",
         "image": require("../assets/images/littleens2.png"),
         "link": "https://sarthakgogna5.wixsite.com/website/work?wix-vod-video-id=f0255ada56264333a915460fbfb3afa6&wix-vod-comp-id=comp-jiwy9miy",
@@ -90,7 +98,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 11,
+        "id": 12,
         "title": "Littleens Campaign Video (Girls) ",
         "image": require("../assets/images/littleens3.png"),
         "link": "https://sarthakgogna5.wixsite.com/website/work?wix-vod-video-id=f0255ada56264333a915460fbfb3afa6&wix-vod-comp-id=comp-jiwy9miy",
@@ -98,7 +106,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 12,
+        "id": 13,
         "title": "Littleens",
         "image": require("../assets/images/littleens4.png"),
         "link": "https://sarthakgogna5.wixsite.com/website/work?wix-vod-video-id=f0255ada56264333a915460fbfb3afa6&wix-vod-comp-id=comp-jiwy9miy",
@@ -106,7 +114,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 13,
+        "id": 14,
         "title": "Peppy Prints",
         "image": require("../assets/images/pp1.png"),
         "link": "https://sarthakgogna5.wixsite.com/website/work?wix-vod-video-id=f0255ada56264333a915460fbfb3afa6&wix-vod-comp-id=comp-jiwy9miy",
@@ -114,7 +122,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 14,
+        "id": 15,
         "title": "Jab Hum Mile - Part 1 - Hunny Sharma",
         "image": require("../assets/images/hs1.jpg"),
         "link": "https://youtu.be/uFN71rziKdc",
@@ -122,7 +130,7 @@ const projects = [
         "Job": "Director of Photography",
     },
     {
-        "id": 15,
+        "id": 16,
         "title": "Jab Hum Mile - Part 2 - Hunny Sharma",
         "image": require("../assets/images/hs2.jpg"),
         "link": "https://youtu.be/hyRkD0KHrLk",
